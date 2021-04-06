3489dc6fa8ab5f2251eb14b554c7f3ee680bfc5c
ahmetdurmaz <ahmet25durmaz@gmail.com>
2021-04-06 23:13:38 +0300

    Ikinci Git deneme proje

--- src/libraryproject/Library.java
+++ src/libraryproject/Library.java
@@ -9,7 +9,7 @@ public class Library {
 	public static List<String> kitapAdi = new ArrayList<>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Ahmet");
 
 	}
 
